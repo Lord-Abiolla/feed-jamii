34bc94ccdd40f66c8f28180db947bbf53532e7fc
Lord-Abiolla <abiollajames@protonmail.com>
2025-10-01 13:25:03 +0300

    Add routing logic for different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import Body from "./components/Body";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import SignUp from "./pages/auth/SignUp";
+import SignIn from "./pages/auth/SignIn";
+import NotFound from "./pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFound />
+  },
+
+  {
+    path: "/signup",
+    element: <SignUp />
+  },
+
+  {
+    path: "signin",
+    element: <SignIn />
+  }
+]);
 
 function App() {
 
-  return (
-    <>
-      <Body />
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
