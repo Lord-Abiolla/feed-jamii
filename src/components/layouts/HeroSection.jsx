e7b6da3ea3b87a9d68ce51cb5ed32eba3f541898
Lord-Abiolla <abiollajames@protonmail.com>
2025-10-01 13:26:24 +0300

    Connect 'connect' button to navigate to Sign up page

--- src/components/layouts/HeroSection.jsx
+++ src/components/layouts/HeroSection.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import map from '../../assets/map.jpg';
 
 function HeroSection() {
@@ -11,11 +12,13 @@ function HeroSection() {
                 <div className="absolute bg-[#C3D9AC]/65 flex flex-col p-9 rounded-4xl items-center justify-center text-center w-[90%] sm:w-[60%] md:w-[40%] h-80 z-10">
                     <h2 className='font-merienda text-2xl sm:text-3xl md:text-4xl'>
                         Do you have surplus food <br />
-                        and somebody lacks?
+                        while somebody lacks?
                     </h2>
-                    <button className='font-merienda bg-[#1C3C25] text-white px-5 py-2 cursor-pointer mt-9 rounded-2xl'>
-                        Connect here
-                    </button>
+                    <Link to={"/signup"}>
+                        <button className='font-merienda bg-[#1C3C25] text-white px-5 py-2 cursor-pointer mt-9 rounded-2xl'>
+                            Connect here
+                        </button>
+                    </Link>
                     <p className='mt-9 text-gray-700 text-sm'>Your information is private and secure</p>
                 </div>
             </div>
