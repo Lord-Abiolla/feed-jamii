139e63cf01dd3801204e53fd84dc7b00b8d6e470
Lord-Abiolla <abiollajames@protonmail.com>
2025-10-02 14:26:00 +0300

    Beautify the error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,11 @@
 function NotFound() {
     return (
-        <>
-            <h1>Not found</h1>
-            <button>Go back</button>
-        </>
+        <div className="place-items-center gap-4 text-2xl text-[#30573B] bg-[#EFF5E9] w-2xl">
+            <div className="flex flex-col items-center p-3">
+                <h1>Not found</h1>
+                <button>Go back</button>
+            </div>
+        </div>
     )
 }
 
