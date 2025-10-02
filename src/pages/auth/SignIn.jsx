94d0bdc9cce232ebee3a52c4e756da52ecd4cd66
Lord-Abiolla <abiollajames@protonmail.com>
2025-10-02 22:30:57 +0300

    Implement login logic and redirect to dahsboard

--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -1,16 +1,68 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
 
 function SignIn() {
+
+    const [formData, setFormData] = useState({
+        username: "",
+        password: ""
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const stored = localStorage.getItem("users");
+        const users = stored ? JSON.parse(stored) : [];
+        const existingUser = users.find(user => user.username === formData.username && user.password === formData.password);
+
+        if (existingUser) {
+            navigate("/dashboard")
+        } else {
+            throw new Error("Invalid username or password!");
+        }
+    }
+
     return (
         <div className="grid bg-[#EFF5E9] h-screen place-items-center">
             <div className="bg-[#C3D9AC] mx-auto w-l shadow rounded-3xl p-4 text-center">
                 <h1 className="font-bold text-2xl mb-4 mt-7">Sign In</h1>
 
-                <div className="flex flex-col items-center p-3">
-                    <input className="bg-[#EFF5E9] rounded-2xl w-80 p-4 m-3 shadow-xl outline-none" type="text" placeholder="Username" required />
-                    <input className="bg-[#EFF5E9] rounded-2xl w-80 p-4 m-3 shadow-xl outline-none" type="password" placeholder="Password" required />
+                <form
+                    className="flex flex-col items-center p-3"
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                        className="bg-[#EFF5E9] rounded-2xl w-80 p-4 m-3 shadow-xl outline-none"
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        onChange={handleChange}
+                        required />
+                    <input
+                        className="bg-[#EFF5E9] rounded-2xl w-80 p-4 m-3 shadow-xl outline-none"
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        onChange={handleChange}
+                        required />
 
-                    <button className="bg-[#30573B] rounded-4xl w-70 text-white font-bold p-3 mt-4 cursor-pointer shadow-2xl">
+                    <button
+                        className="bg-[#30573B] rounded-4xl w-70 text-white font-bold p-3 mt-4 cursor-pointer shadow-2xl"
+                        type="submit"
+                    >
                         Sign In
                     </button>
 
@@ -22,7 +74,7 @@ function SignIn() {
                             Sign up here
                         </Link>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     )
