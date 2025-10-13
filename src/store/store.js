dae8ee717dc8f2c1eb0c1b7f9a98d45dd0088172
Lord-Abiolla <abiollajames@protonmail.com>
2025-10-13 17:40:58 +0300

    Update zustand store to capture selected restaurants

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,8 @@ import { create } from "zustand";
 
 const useMapStore = create((set) => ({
     userLocation: null,
-    selectedDestination: null,
-    nearbyPlaces: [],
-    route: null,
-
-    setUserLocation: (coords) => set({ userLocation: coords }),
-    setNearbyPlaces: (place) => set({ nearbyPlaces: place }),
-    setSelectedDestination: (places) => set({ selectedDestination: places }),
-    setRoute: (geojson) => set({ route: geojson }),
+    selectedRestaurant: null,
+    setUserLocation: (loc) => set({ userLocation: loc }),
+    setSelectedRestaurant: (restaurant) => set({ selectedRestaurant: restaurant }),
 }));
 export default useMapStore;
