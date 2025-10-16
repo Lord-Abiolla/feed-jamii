4917ecb8aa4ae49415ed42933b576176ee42e9e7
Lord-Abiolla <abiollajames@protonmail.com>
2025-10-16 18:18:43 +0300

    Update the haeder buttons to navigate to signup and signin pages

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
     return (
         <div className="top-4 flex gap-9 items-center justify-between border-1 border-green-700 bg-[#d0dfc0] mb-4 mx-4 rounded-full shadow-2xl sticky z-[1000]">
@@ -7,14 +9,18 @@ function Header() {
                 </h1>
             </div>
             <div className="p-2 flex gap-4 mr-30">
-                <button
-                    className="font-merienda border-2 border-[#1C3C25] bg-transparent px-3 w-30 cursor-pointer rounded-3xl font-semibold text-center p-3 text-[#1C3C25] hover:bg-[#1C3C25] hover:text-white"
-                >
-                    Sign In
-                </button>
-                <button className="font-merienda bg-green-900 px-3 w-40 cursor-pointer rounded-3xl font-semibold text-center p-3 text-white hover:bg-transparent hover:text-[#1C3C25] hover:border-2 hover:border-[#1C3C25]">
-                    Get started
-                </button>
+                <Link to={"/signin"}>
+                    <button
+                        className="font-merienda border-2 border-[#1C3C25] bg-transparent px-3 w-30 cursor-pointer rounded-3xl font-semibold text-center p-3 text-[#1C3C25] hover:bg-[#1C3C25] hover:text-white"
+                    >
+                        Sign In
+                    </button>
+                </Link>
+                <Link to={"/signup"}>
+                    <button className="font-merienda bg-green-900 px-3 w-40 cursor-pointer rounded-3xl font-semibold text-center p-3 text-white hover:bg-transparent hover:text-[#1C3C25] hover:border-2 hover:border-[#1C3C25]">
+                        Get started
+                    </button>
+                </Link>
             </div>
             {/* <hr className="mt-2 bg-gray-400" /> */}
         </div>
