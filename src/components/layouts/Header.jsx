cd7fd67845414798b08e20a88a3b0fc58ed36da1
Lord-Abiolla <abiollajames@protonmail.com>
2025-10-16 18:14:28 +0300

    Update header to incorporate sign in and Get Started buttons

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,10 +1,22 @@
 function Header() {
     return (
-        <div className="top-0">
-            <h1 className="font-merienda text-5xl font-semibold text-center p-3 text-[#1C3C25]">
-                feed-Jamii
-            </h1>
-            <hr className="mt-2 bg-gray-400" />
+        <div className="top-4 flex gap-9 items-center justify-between border-1 border-green-700 bg-[#d0dfc0] mb-4 mx-4 rounded-full shadow-2xl sticky z-[1000]">
+            <div className="p-2">
+                <h1 className="ml-30 font-merienda text-4xl font-semibold text-center p-3 text-[#1C3C25]">
+                    feed-Jamii
+                </h1>
+            </div>
+            <div className="p-2 flex gap-4 mr-30">
+                <button
+                    className="font-merienda border-2 border-[#1C3C25] bg-transparent px-3 w-30 cursor-pointer rounded-3xl font-semibold text-center p-3 text-[#1C3C25] hover:bg-[#1C3C25] hover:text-white"
+                >
+                    Sign In
+                </button>
+                <button className="font-merienda bg-green-900 px-3 w-40 cursor-pointer rounded-3xl font-semibold text-center p-3 text-white hover:bg-transparent hover:text-[#1C3C25] hover:border-2 hover:border-[#1C3C25]">
+                    Get started
+                </button>
+            </div>
+            {/* <hr className="mt-2 bg-gray-400" /> */}
         </div>
     )
 }
