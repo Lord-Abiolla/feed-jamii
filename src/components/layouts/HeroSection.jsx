453d97a204756775d0cd4a8b514390bdabe87554
Lord-Abiolla <abiollajames@protonmail.com>
2025-10-17 16:32:57 +0300

    Update the Hero section to look appealing with clear typography

--- src/components/layouts/HeroSection.jsx
+++ src/components/layouts/HeroSection.jsx
@@ -3,20 +3,24 @@ import map from '../../assets/map.jpg';
 
 function HeroSection() {
     return (
-        <div className='relative w-full h-[700px]'>
-            {/* Background image */}
-            <img src={map} alt="map" className="absolute object-cover w-full h-[700px] p-6 rounded-[7rem]" />
+        <div className='relative w-full h-[700px] mt-12'>
+            <img src={map} alt="map" className="absolute object-cover w-full h-[750px] blur-[1.5px]" />
 
             {/* Overlay card */}
             <div className='relative flex items-center justify-center h-full'>
-                <div className="absolute bg-[#C3D9AC]/65 flex flex-col p-9 rounded-4xl items-center justify-center text-center w-[90%] sm:w-[60%] md:w-[40%] h-80 z-10">
-                    <h2 className='font-merienda text-2xl sm:text-3xl md:text-4xl'>
-                        Do you have surplus food <br />
-                        while somebody lacks?
+                <div className="absolute flex flex-col items-center justify-center text-center z-10">
+                    <h2 className='font-merienda text-[60px] font-bold text-green-700 [text-shadow:_7px_7px_0_#1C3C25]'>
+                        feed-Jamii
                     </h2>
+                    <p className='font-merienda text-[#07140a] text-[27px] mt-9'>
+                        A community-driven web app that connects individuals, <br />
+                        organizations, and donors to fight hunger by sharing <br />
+                        food and essential resources within<br />
+                        local communities.
+                    </p>
                     <Link to={"/signup"}>
-                        <button className='font-merienda bg-[#1C3C25] text-white px-5 py-2 cursor-pointer mt-9 rounded-2xl'>
-                            Connect here
+                        <button className='font-merienda bg-white border-2 border-[#1C3C25] text-[#1C3C25] text-2xl font-bold px-14 py-5 cursor-pointer mt-9 rounded-full hover:bg-[#1C3C25] hover:text-white'>
+                            Get started here
                         </button>
                     </Link>
                     <p className='mt-9 text-gray-700 text-sm'>Your information is private and secure</p>
